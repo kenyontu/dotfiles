f15e4db53ae0290ba197642d538391e273481ad2
kenyontu <kenyontu@outlook.com>
2022-08-13 10:46:21 +0800

    add snippets

--- install.sh
+++ install.sh
@@ -13,3 +13,9 @@ ln -sv ~/dotfiles/kitty/theme.conf ~/.config/kitty/
 mkdir ~/.config/nvim
 ln -sv ~/dotfiles/nvim/init.vim ~/.config/nvim/
 ln -sv ~/dotfiles/nvim/coc-settings.json ~/.config/nvim/
+
+mkdir ~/.vim/vscode-snippets
+ln -sv ~/dotfiles/.vim/vscode-snippets/js.code-snippets ~/.vim/vscode-snippets/js.code-snippets
+ln -sv ~/dotfiles/.vim/vscode-snippets/react.code-snippets ~/.vim/vscode-snippets/react.code-snippets
+ln -sv ~/dotfiles/.vim/vscode-snippets/redux.code-snippets ~/.vim/vscode-snippets/redux.code-snippets
+ln -sv ~/dotfiles/.vim/vscode-snippets/testing.code-snippets ~/.vim/vscode-snippets/testing.code-snippets
