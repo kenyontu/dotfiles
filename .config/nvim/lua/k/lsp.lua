e537550c2b9da0d8ed44edccd6c5bc053428b891
kenyontu <kenyontu@outlook.com>
2022-10-30 19:39:33 +0800

    add prisma to lsp config

--- .config/nvim/lua/k/lsp.lua
+++ .config/nvim/lua/k/lsp.lua
@@ -56,6 +56,8 @@ lspconfig.html.setup {
 
 lspconfig['cssls'].setup { capabilities = capabilities }
 
+lspconfig["prismals"].setup {}
+
 vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(
   vim.lsp.diagnostic.on_publish_diagnostics, {
     underline = true,
