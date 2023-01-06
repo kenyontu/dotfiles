6ddb02e6ec77ed83c9df03e8390332c0d4b7ca1e
kenyontu <kenyontu@outlook.com>
2023-01-06 22:38:47 +0800

    fix tab not working

--- .config/nvim/lua/k/cmp.lua
+++ .config/nvim/lua/k/cmp.lua
@@ -25,12 +25,16 @@ cmp.setup({
     ["<Tab>"] = cmp.mapping(function(fallback)
       if cmp.visible() then
         cmp.select_next_item()
+      else
+        fallback()
       end
     end, { "i", "s" }),
 
     ["<S-Tab>"] = cmp.mapping(function(fallback)
       if cmp.visible() then
         cmp.select_prev_item()
+      else
+        fallback()
       end
     end, { "i", "s" }),
   }),
