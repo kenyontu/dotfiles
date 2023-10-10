b60693cc4c0c55b7330db835e7b7d560aac08d9c
kenyontu <kenyontu@outlook.com>
2023-10-10 15:30:11 +0800

    format code with conform.nvim

---
+++ .config/nvim/lua/plugins/conform.lua
@@ -0,0 +1,41 @@
+return {
+  'stevearc/conform.nvim',
+  config = function()
+    require("conform").setup({
+      formatters_by_ft = {
+        javascript = { { "prettierd", "prettier" } },
+        javascriptreact = { { "prettierd", "prettier" } },
+        typescript = { { "prettierd", "prettier" } },
+        typescriptreact = { { "prettierd", "prettier" } },
+        json = { { "prettierd", "prettier" } },
+        markdown = { { "prettierd", "prettier" } },
+        toml = { { "prettierd", "prettier" } },
+        html = { { "prettierd", "prettier" } },
+        css = { { "prettierd", "prettier" } },
+        scss = { { "prettierd", "prettier" } },
+        graphql = { { "prettierd", "prettier" } },
+        svelte = { { "prettierd", "prettier" } },
+        rust = { "rustfmt" },
+        css = { { "prettierd", "prettier" } },
+        dart = { "dart_fmt" }
+      },
+      formatters = {
+        rustfmt = {
+          meta = {
+            url = "https://github.com/rust-lang/rustfmt",
+            description = "A tool for formatting Rust code according to style guidelines.",
+          },
+          command = "rustfmt",
+          args = { "--edition", "2021" } 
+        }
+      }
+    }) 
+
+    vim.api.nvim_create_autocmd("BufWritePre", {
+      pattern = "*",
+      callback = function(args)
+        require("conform").format({ bufnr = args.buf })
+      end
+    })
+  end
+}
--- .config/nvim/lua/plugins/null-ls.lua
+++ .config/nvim/lua/plugins/null-ls.lua
@@ -10,44 +10,7 @@ return {
         nlsb.diagnostics.eslint,
         nlsb.diagnostics.write_good, -- for markdown
         nlsb.diagnostics.jsonlint,
-
-        -- formatting
-        nlsb.formatting.prettierd.with({
-          filetypes = {
-            "javascript",
-            "javascriptreact",
-            "typescript",
-            "typescriptreact",
-            "json",
-            "markdown",
-            "toml",
-            "css",
-            "scss",
-            "less",
-            "html",
-            "graphql",
-            "vue"
-          },
-          extra_filetypes = { 
-            "svelte",
-          },
-        }),
-        nlsb.formatting.rustfmt, -- rust
-        nlsb.formatting.autopep8, -- python
-        nlsb.formatting.dart_format,
       },
-      on_attach = function(client, bufnr)
-        if client.supports_method("textDocument/formatting") then
-          vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
-          vim.api.nvim_create_autocmd("BufWritePre", {
-            group = augroup,
-            buffer = bufnr,
-            callback = function()
-              vim.lsp.buf.format({ bufnr = bufnr })
-            end,
-          })
-        end
-      end,
     })
   end
 }
