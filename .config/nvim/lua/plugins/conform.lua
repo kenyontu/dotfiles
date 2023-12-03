3b9a89dbe503a433355a37a7ae21d260d410873f
Kenyon Tu <kenyontu@outlook.com>
2023-12-03 19:30:08 +0800

    update nvim

--- .config/nvim/lua/plugins/conform.lua
+++ .config/nvim/lua/plugins/conform.lua
@@ -34,7 +34,7 @@ return {
     vim.api.nvim_create_autocmd("BufWritePre", {
       pattern = "*",
       callback = function(args)
-        require("conform").format({ bufnr = args.buf })
+        require("conform").format({ bufnr = args.buf, quiet = true })
       end
     })
   end
